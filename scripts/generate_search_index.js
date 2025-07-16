2af551d17a6d61b07e45483421be91ab805192a9
Bjoern Siepe <bjoernsiepe@gmail.com>
2025-07-16 14:59:29 +0200

    enable new search functionality //
    added constructs, search bar to the left

--- scripts/generate_search_index.js
+++ scripts/generate_search_index.js
@@ -77,6 +77,7 @@ const processDatasets = () => {
         n_beeps_per_day: formatBeepsPerDay(data.n_beeps_per_day),
         variables: data.features.map(feature => ({
         name: feature.name,
+        description: feature.description,
         type: feature.variable_type,
         coding: feature.coding,
         answer_categories: feature.answer_categories,
